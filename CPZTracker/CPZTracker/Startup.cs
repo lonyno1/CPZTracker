e973644f03c4403ebebec9544fe0ae1e3fd99640
lonyno1 <projectprogramming@gmail.com>
2017-12-19 14:13:16 +0600

    CPZ Tracker MVC Web Application Initialized

---
+++ CPZTracker/CPZTracker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CPZTracker.Startup))]
+namespace CPZTracker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
